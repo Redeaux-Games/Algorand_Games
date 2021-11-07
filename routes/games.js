181c2b39fe631ba07e6f01be1d4551c0cf247ec0
Michael Hernandez <admin@highplainsdesignstudio.com>
2021-11-07 15:43:08 -0700

    added game route parameters to auto load games.

--- routes/games.js
+++ routes/games.js
@@ -5,8 +5,12 @@ router.get('/', function(req, res, next) {
     res.send("You're at the games.")
 });
 
-router.get('/HireForceMax', function(req, res, next) {
-    res.render('games/HireForceMax', {title:"Hire Force Max Title"})
+router.get("/:game", function(req, res, next) {
+    res.render("games/index", {title: req.params.game})
 })
+
+// router.get('/HireForceMax', function(req, res, next) {
+//     res.render('games/HireForceMax', {title:"Hire Force Max Title"})
+// })
 
 module.exports = router;
